8f0ee8d65a140a15d6fd84ccf18ab777e07e9b9d
Arlequin <arlequine666.ah@gmail.com>
2017-12-26 11:15:58 -0600

    Authentication part-1

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,7 +1,9 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
-  field :CreateLink, function: Resolvers::CreateLink.new
+  field :createLink, function: Resolvers::CreateLink.new
+  field :createUser, function: Resolvers::CreateUser.new
+  field :signinUser, function: Resolvers::SignInUser.new
   # queries are just represented as fields
   field :allLinks, !types[Types::LinkType] do
     # resolve would be called in order to fetch data for that field
